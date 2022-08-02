9e8900fba5e3bbaef31479f603883e004ed9754d
Tony-Shnip <shnip12345@gmail.com>
2022-08-02 13:46:31 +0300

    init: start project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
